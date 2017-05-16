ace5e857c84f52c68e0cfaec6adde8ce2fce1fa0
militarpancho <militarpancho_93@msn.com>
2017-05-16 13:55:25 +0200

    Redefined collector input interface

--- collector/collector.py
+++ collector/collector.py
@@ -8,17 +8,41 @@ class esCollector(Elasticsearch):
     def __init__(self, hosts, odl_endpoint='http://localhost:8181'):
         super(esCollector, self).__init__(hosts=hosts)
         self.odl = ODLClient(odl_endpoint)
-        self.id_count = 0
 
-    def add_simulationData(self):
+    def add_simulation(self, simulation_id, start_date=None):
         data = {
             'network-topology': self.odl.get_networkTopology(),
             'inventory': self.odl.get_inventory(),
-            'timestamp': datetime.now()
+            'timestamp': datetime.now(),
+            'start_date': start_date
         }
         resp = self.index(
-            index="simulations",
-            id=self.id_count,
+            index="simulation{}".format(simulation_id),
             doc_type='simulation',
             body=data)
-        self.id_count += 1
+
+    def add_action(self, simulation_id, action_name, action_id):
+
+        data = {
+            'inventory': self.odl.get_inventory(),
+            'timestamp': datetime.now(),
+            'action_name': action_name,
+            'action_id': action_id
+        }
+
+        resp = self.index(
+            index="simulation{}".format(simulation_id),
+            doc_type='simulation',
+            body=data)
+
+    def add_simulationFinish(self, simulation_id, end_date=None):
+
+        data = {
+            'inventory': self.odl.get_inventory(),
+            'timestamp': datetime.now(),
+            'end_date': end_date
+        }
+        resp = self.index(
+            index="simulation{}".format(simulation_id),
+            doc_type='simulation',
+            body=data)
