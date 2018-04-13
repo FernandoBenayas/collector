c9c88e25e49909117448ad654115a3970b375754
Fernando Benayas <f.benayas@alumnos.upm.es>
2018-04-13 10:04:48 +0200

    Added author fields

--- collector/__init__.py
+++ collector/__init__.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 
+# -*- coding: utf-8 -*-
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#
+# Copyright (c) 2018  Manuel García-Amado  <militarpancho@gmail.com>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#title           : __init__.py
+#date created    : 22/01/2018
+#python_version  : 3.5.1
+#notes           :
+__author__ = "Manuel García-Amado"
+__license__ = "GPLv2"
+__version__ = "0.1.0"
+__maintainer__ = "Manuel García-Amado"
+__email__ = "militarpancho@gmail.com"
+
+"""This program can change the license header inside files.
+"""
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
 """
 Collector module for sdn diagnostic module
 
-__author__ = "GSI"
 """
 
 import logging
